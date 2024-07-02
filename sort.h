c57e643d7ff223a3243ba0c0327c9b0e44b89888
ChengChaoChun <abc21531050@gmail.com>
2024-07-02 13:58:56 +0800

    Complete the write system call
    Use the write system call to choose the sorting algorithm, offering
    three options: qsort, pdqsort, and timsort.

--- sort.h
+++ sort.h
@@ -9,4 +9,11 @@ extern struct workqueue_struct *workqueue;
 
 void sort_main(void *sort_buffer, size_t size, size_t es, cmp_t cmp);
 
+typedef void (*swap_func_t)(void *a, void *b, int size);
+
+typedef int (*cmp_r_func_t)(const void *a, const void *b, const void *priv);
+typedef int (*cmp_func_t)(const void *a, const void *b);
+
+void sort_pdqsort(void *base, size_t num, size_t size, cmp_func_t cmp_func, swap_func_t swap_func);
+
 #endif
--- user.c
+++ user.c
@@ -1,12 +1,17 @@
+/*
+ * parameter   1 : timsort , 2 : linuxsort , 3 : pdqsort , 4 : qosrt  
+ */
+
 #include <fcntl.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <stdint.h>
 
 #define KSORT_DEV "/dev/sort"
 
-int main()
+int main(int argc, char *argv[])
 {
     int fd = open(KSORT_DEV, O_RDWR);
     if (fd < 0) {
@@ -15,19 +20,40 @@ int main()
     }
 
     size_t n_elements = 1000;
-    size_t size = n_elements * sizeof(int);
-    int *inbuf = malloc(size);
+    size_t size = n_elements * sizeof(uint64_t);
+    uint64_t *inbuf = malloc(size);
     if (!inbuf)
         goto error;
 
     for (size_t i = 0; i < n_elements; i++)
         inbuf[i] = rand() % n_elements;
 
-    ssize_t r_sz = read(fd, inbuf, size);
-    if (r_sz != size) {
+
+    int *set_sort = malloc(sizeof(int));
+    *set_sort = atoi(argv[1]);
+    if (*set_sort == 2) {
+        printf("sort by pdqsort\n");
+    } else if (*set_sort == 3) {
+        printf("sort by linux lib sort\n");
+    } else {
+        printf("sort by qsort\n");
+    }
+    ssize_t set_res = write(fd, set_sort, sizeof(int));
+    if (set_res > 0) {
+        printf("set seccuess  %zd\n", set_res);
+    } else {
+        printf("set failed\n");
+    }
+    free(set_sort);
+
+
+    ssize_t r_time = read(fd, inbuf, size);
+    if (r_time <= 0) {
         perror("Failed to read character device");
         goto error;
     }
+    printf("sorting time : %ld\n", r_time);
+
 
     bool pass = true;
     int ret = 0;
